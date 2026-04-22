07329e2fab11ebfe37744e72a015a5a90cfa9844
MLRIT Dev <dev@mlrit.ac.in>
2026-04-22 21:35:00 +0530

    chore: remove legacy build/fix scripts from root

--- cse_urls.js
+++
@@ -1,4 +0,0 @@
-var syllabusUrls = {
-        btech: 'https://files.mlrit.ac.in/uploads/R25%20Syllabus/R25_CSE-Syllabus.pdf',
-        mtech: 'https://mlrit.ac.in/curriculum/cse-m-tech-r22-syllabus/'
-      };
