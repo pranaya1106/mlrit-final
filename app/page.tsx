2a00dc5ec5e280679f643ecab801717e6307d86d
Naga Sai Chinmayee <nnschinmayee07@gmail.com>
2026-06-05 03:25:45 +0530

    fix: move Testimonials (alumni) above Events on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function HomePage() {
       <SuccessStories />
       <Programs />
       <Placements />
-      <Events />
       <Testimonials />
+      <Events />
     </>
   );
 }
